4acdbf2a33f2049feedbf33a54244d64018610b2
RJ Luengas <robertojla8@gmail.com>
2024-06-25 12:59:48 -0600

    Se creo el archivo constants.dart Se utilizan constantes en el diseño y se usa un ejemplo con MediaQuery.of(context).size;

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFEB0029);
+const kSecondaryColor = Color(0xFFF5F5F5);
+const kTertiaryColor = Color(0xFFC6C9C9);
+const kQuaternaryColor = Color(0xFF6A6867);
--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -3,21 +3,39 @@ import 'package:flutter/material.dart';
 //GoRouter:
 import 'package:go_router/go_router.dart';
 
+//Constants:
+import 'package:rive_and_flutter/constants.dart';
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('PROFILE PAGE'),
       ),
       body: Center(
-        child: OutlinedButton(
-          child: const Text('Editar perfil'),
-          onPressed: () {
-            context.goNamed('Edit profile');
-          },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              backgroundColor: kTertiaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              minimumSize: Size(screenSize.width * 0.8, 50),
+            ),
+            child: const Text(
+              'Editar perfil',
+              style: TextStyle(color: kSecondaryColor),
+            ),
+            onPressed: () {
+              context.goNamed('Edit profile');
+            },
+          ),
         ),
       ),
     );
